27eb60238ca22f1711f4aebd71bdef75b87fe31c
cbrlsn <clemens.burleson@gmail.com>
2024-12-10 08:10:17 +0100

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#### Defining general properties of the app
+###########################################
+st.set_page_config(
+    page_title="Diamond Brothers",
+    page_icon='💎',
+    layout='wide'
